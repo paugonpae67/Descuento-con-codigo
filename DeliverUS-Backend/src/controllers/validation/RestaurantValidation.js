7120a6bbcf841db9dc73a77bfff9a29d92189755
paula <paulagonzalezllanes@gmail.com>
2024-05-23 13:20:54 +0200

    first commit

---
+++ DeliverUS-Backend/src/controllers/validation/RestaurantValidation.js
@@ -0,0 +1,74 @@
+import { check } from 'express-validator'
+import { checkFileIsImage, checkFileMaxSize } from './FileValidationHelper.js'
+import { Restaurant } from '../../models/models.js'
+const maxFileSize = 2000000 // around 2Mb
+
+const checkNotExistDescuento = async (value, { req }) => {
+  try {
+    const restaurant = await Restaurant.findOne({
+      where: {
+        userId: req.user.id,
+        codigo: req.body.codigo
+      }
+    })
+    if (restaurant !== null) {
+      return Promise.reject(new Error('Thre is a restaurant with this codigo'))
+    } else { return Promise.resolve() }
+  } catch (err) {
+    return Promise.reject(new Error(err))
+  }
+}
+const create = [
+  check('name').exists().isString().isLength({ min: 1, max: 255 }).trim(),
+  check('description').optional({ nullable: true, checkFalsy: true }).isString().trim(),
+  check('address').exists().isString().isLength({ min: 1, max: 255 }).trim(),
+  check('postalCode').exists().isString().isLength({ min: 1, max: 255 }),
+  check('url').optional({ nullable: true, checkFalsy: true }).isString().isURL().trim(),
+  check('shippingCosts').exists().isFloat({ min: 0 }).toFloat(),
+  check('email').optional({ nullable: true, checkFalsy: true }).isString().isEmail().trim(),
+  check('phone').optional({ nullable: true, checkFalsy: true }).isString().isLength({ min: 1, max: 255 }).trim(),
+  check('restaurantCategoryId').exists({ checkNull: true }).isInt({ min: 1 }).toInt(),
+  check('userId').not().exists(),
+  check('descuento').optional().isInt({ min: 0, max: 100 }).toInt(),
+  check('codigo').optional().isString().trim().custom(checkNotExistDescuento),
+  check('heroImage').custom((value, { req }) => {
+    return checkFileIsImage(req, 'heroImage')
+  }).withMessage('Please upload an image with format (jpeg, png).'),
+  check('heroImage').custom((value, { req }) => {
+    return checkFileMaxSize(req, 'heroImage', maxFileSize)
+  }).withMessage('Maximum file size of ' + maxFileSize / 1000000 + 'MB'),
+  check('logo').custom((value, { req }) => {
+    return checkFileIsImage(req, 'logo')
+  }).withMessage('Please upload an image with format (jpeg, png).'),
+  check('logo').custom((value, { req }) => {
+    return checkFileMaxSize(req, 'logo', maxFileSize)
+  }).withMessage('Maximum file size of ' + maxFileSize / 1000000 + 'MB')
+]
+const update = [
+  check('name').exists().isString().isLength({ min: 1, max: 255 }).trim(),
+  check('description').optional({ nullable: true, checkFalsy: true }).isString().trim(),
+  check('address').exists().isString().isLength({ min: 1, max: 255 }).trim(),
+  check('postalCode').exists().isString().isLength({ min: 1, max: 255 }),
+  check('url').optional({ nullable: true, checkFalsy: true }).isString().isURL().trim(),
+  check('shippingCosts').exists().isFloat({ min: 0 }).toFloat(),
+  check('email').optional({ nullable: true, checkFalsy: true }).isString().isEmail().trim(),
+  check('phone').optional({ nullable: true, checkFalsy: true }).isString().isLength({ min: 1, max: 255 }).trim(),
+  check('restaurantCategoryId').exists({ checkNull: true }).isInt({ min: 1 }).toInt(),
+  check('userId').not().exists(),
+  check('descuento').optional().isInt({ min: 0, max: 100 }).toInt(),
+  check('codigo').optional().isString().custom(checkNotExistDescuento),
+  check('heroImage').custom((value, { req }) => {
+    return checkFileIsImage(req, 'heroImage')
+  }).withMessage('Please upload an image with format (jpeg, png).'),
+  check('heroImage').custom((value, { req }) => {
+    return checkFileMaxSize(req, 'heroImage', maxFileSize)
+  }).withMessage('Maximum file size of ' + maxFileSize / 1000000 + 'MB'),
+  check('logo').custom((value, { req }) => {
+    return checkFileIsImage(req, 'logo')
+  }).withMessage('Please upload an image with format (jpeg, png).'),
+  check('logo').custom((value, { req }) => {
+    return checkFileMaxSize(req, 'logo', maxFileSize)
+  }).withMessage('Maximum file size of ' + maxFileSize / 1000000 + 'MB')
+]
+
+export { create, update }
